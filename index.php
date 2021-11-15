17a2d2ccbb4496d37f9e2460d29bb4bdef3d277c
Ivo Schwizer <ivoschwizer@gmail.com>
2021-11-15 15:40:58 +0100

    start

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<!--[if IE 7]>	 <html class="no-js lt-ie9 lt-ie8" lang="de"><![endif]-->
+<!--[if IE 8]>	 <html class="no-js lt-ie9" lang="de"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="de">
+<!--<![endif]-->
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
+    <meta charset="utf-8">
+    <title>Art of Loft</title>
+    <meta name="description" content="Art of Loft">
+    <?php include 'head.php'; ?>
+</head>
+
+<body>
+    <section class="container">
+      <div class="logo">
+        <a href=""><img src="img/artofloft-logo.svg" alt="Art of Loft Logo" width="180px" height="auto"></a>
+        <a class="kontakt" href="#footer" >Kontakt</a>
+      </div>
+      <article class="projekt right first">
+        <img src="img/art-of-loft-neubau-8-oeserigi-appenzell-innerhoden.jpg" alt="art-of-loft-neubau-8-oeserigi-appenzell-innerhoden" width="100%" height="auto">
+        <h2>Neubau<br>Realisation ab 2022</h2>
+        <h1>8 Öserigi, 8 MFH<br>Appenzell AI</h1>
+      </article>
+      <article class="projekt center">
+        <img src="img/art-of-loft-neubau-segnas.jpg" alt="art-of-loft-neubau-segnas" width="100%" height="auto">
+        <h2>Neubau<br>Realisation ab 2022</h2>
+        <h1>Chalet mit 4 WHG<br>Segnas GR</h1>
+      </article>
+      <article class="projekt full">
+        <img src="img/art-of-loft-neubau-polygon-bottighofen.jpg" alt="art-of-loft-neubau-polygon-bottighofen" width="100%" height="auto">
+        <h2>Neubau<br>Realisation ab 2022</h2>
+        <h1>Polygon, Gewerbebau<br>Bottighofen TG</h1>
+      </article>
+      <article class="projekt right">
+        <img src="img/art-of-loft-neubau-amriswil.jpg" alt="art-of-loft-neubau-amriswil" width="100%" height="auto">
+        <h2>Neubau<br>Realisation ab 2020</h2>
+        <h1>3 MFH<br>Amriswil TG</h1>
+      </article>
+      <article class="projekt center">
+        <img src="img/art-of-loft-studie-mfh-wattwil.jpg" alt="art-of-loft-studie-mfh-wattwil" width="100%" height="auto">
+        <h2>Studie<br>2020</h2>
+        <h1>MFH<br> Wattwil SG</h1>
+      </article>
+      <article class="projekt full">
+        <img src="img/art-of-loft-studie-villa-spanien.jpg" alt="art-of-loft-studie-villa-spanien" width="100%" height="auto">
+        <h2>Studie<br>2015</h2>
+        <h1>Villa<br>Spanien</h1>
+      </article>
+      <article class="projekt right">
+        <img src="img/art-of-loft-studie-herrliberg.jpg" alt="art-of-loft-studie-herrliberg" width="100%" height="auto">
+        <h2>Eingeladener Wettbewerb<br>Realisation 2019 abgeschlossen</h2>
+        <h1>Herrliberg ZH</h1>
+      </article>
+      <article class="projekt center">
+        <img src="img/art-of-loft-eingeladener-wettbewerb.jpg" alt="art-of-loft-eingeladener-wettbewerb" width="100%" height="auto">
+        <h2>Eingeladener Wettbewerb<br>2014</h2>
+        <h1>Ermatingen TG</h1>
+      </article>
+
+      <div id="footer">
+        <h2 class="pw">
+            16.04.2022<br>
+            Relaunch<br>
+            neue Webseite<br><br>
+        </h2>
+        <h2>
+          <a target="_blank" href="https://www.google.ch/maps/place/Art+of+Loft/@47.4296178,9.3775603,17z/data=!3m1!4b1!4m5!3m4!1s0x479b1e4578691d93:0xb96068c2a06244e9!8m2!3d47.4296961!4d9.3798401">
+            Art of Loft AG<br>
+            Schützengartenweg 3<br>
+            9000 St.Gallen<br><br>
+          </a>
+          <a href="tel:0714140202">T 071 414 02 02</a><br>
+          <a href="mailto:t.von.allmen@art-of-loft.ch" target="_top">Mail</a><br>
+        </h2>
+      </div>
+    </section>
+
+    <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+  $("a[href^='#']").click(function(e) {
+	e.preventDefault();
+
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	}, 1000, 'linear' );
+});
+
+});
